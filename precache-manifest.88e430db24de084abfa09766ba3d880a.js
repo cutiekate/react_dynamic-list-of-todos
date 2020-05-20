6a36f68b9b1032b31195563b225ec4037234cc46
kate <kate@test.com>
2020-05-20 13:38:54 +0300

    Updates

---
+++ precache-manifest.88e430db24de084abfa09766ba3d880a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e73020ab3df30119f47b07042e6008",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef5daab876005c2da27b",
+    "url": "./static/css/main.cd27f40e.chunk.css"
+  },
+  {
+    "revision": "88a9a5f3642db64e2885",
+    "url": "./static/js/2.f30f3ded.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f30f3ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5daab876005c2da27b",
+    "url": "./static/js/main.5cb2a432.chunk.js"
+  },
+  {
+    "revision": "319a3c468d8ecd2a5698",
+    "url": "./static/js/runtime-main.e5e6300a.js"
+  }
+]);
